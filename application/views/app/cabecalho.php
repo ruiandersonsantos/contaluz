6b06e01bd7326305b2bcaa05f0096d2ef8ef50cc
ruiandersonsantos <ruiandersonsantos@gmail.com>
2016-06-13 22:12:01 -0300

    Menu finalizado 13/06/2016

--- application/views/app/cabecalho.php
+++ application/views/app/cabecalho.php
@@ -7,9 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- adicionar CSS Bootstrap -->
         <link href="<?= base_url("css_b/bootstrap.min.css") ?>" rel="stylesheet">  
-        
+
         <link href="<?= base_url("css_b/bootstrap-switch.min.css") ?>" rel="stylesheet">  
-        
+
         <link href="<?= base_url("css_b/highlight.css") ?>" rel="stylesheet">  
         <!-- CSS personalizado -->
         <link href = "<?= base_url("css_b/estilo.css") ?>" rel = "stylesheet" media = "screen">
@@ -17,103 +17,94 @@
         <!-- CSS personalizado -->
         <link href = "<?= base_url("css_b/simple-sidebar.css") ?>" rel = "stylesheet" media = "screen">
 
+        <script src="https://use.fontawesome.com/5ddc58b82b.js"></script>
         <title>Area Administrativa</title>
     </head>
 
     <body>
 
-        <!-- navbar logado -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="navPrincipal">
-            <div class="container-fluid col-xs-12" id="div_container-fluid">
+        <nav class="navbar navbar-default no-margin">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header fixed-brand">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"  id="menu-toggle">
+                    <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+                </button>
+                <a class="navbar-brand" href="#"><i class="fa fa-rocket fa-4"></i> Calcula Conta de Luz</a>
+            </div><!-- navbar-header-->
 
-                <div class="col-xs-2" id="div_menu-toogle">
-                    <a class="navbar-toggle" id="menu-toggle" href="#menu-toggle">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                </div>
-
-                <div class="col-xs-8" id="div_navbar-brand">
-                    <div>
-                        <span class="navbar-brand" id="span_navbar-brand">
-                            CALCULA CONTA
-                        </span>
-                    </div>
-                </div>
-
-                <div class="col-xs-2" id="div_msg_logado">
-                    <?php if ($this->session->userdata("usuario_logado")['origem'] == 'FACEBOOK') : ?>
-                        <img id="img_msg_logado-face" src="<?= $this->session->userdata("usuario_logado")['picture']['data']['url'] ?>"/>
-                    <?php endif ?>
-                        
-                        
-                      <?php if ($this->session->userdata("usuario_logado")['origem'] == 'GOOGLE') : ?>
-                        <img id="img_msg_logado-google" src="<?= $this->session->userdata("usuario_logado")['picture'] ?>"/>
-                    <?php endif ?>   
-                </div>
-
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active" ><button class="navbar-toggle collapse in" data-toggle="collapse" id="menu-toggle-2"> <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span></button></li>
+                </ul>
             </div>
+            <!-- bs-example-navbar-collapse-1 -->
 
-            <!-- wrapper -->
-            <div id="wrapper" >
-                <!-- Sidebar -->
-                <div id="sidebar-wrapper">
-                    <ul class="sidebar-nav" >
-                        <li class="sidebar-brand">
-                            <a href="<?= base_url("/index.php/login") ?>">
-                                BEM VINDO
-                                <span class="glyphicon glyphicon-home " aria-hidden="true"></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url("/index.php/calculo") ?>">
-                                <span class="glyphicon glyphicon-list-alt " aria-hidden="true"></span>
-                                Calculo
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-scale " aria-hidden="true"></span>
-                                Marcação Diária
-                            </a>
-
-                        </li>
-                        <li>
-                            <a href="<?= base_url("/index.php/configuracao") ?>">
-                                <span class="glyphicon glyphicon-cog " aria-hidden="true"></span>
-                                Configurações
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-envelope " aria-hidden="true"></span>
-                                Fale Conosco
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-info-sign " aria-hidden="true"></span>
-                                Sobre
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url("index.php/login/logout") ?>">
-                                <span class="glyphicon glyphicon-log-out " aria-hidden="true"></span>
-                                Sair
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-                <!-- fim - Sidebar -->
-            </div>
-            <!-- fim - wrapper -->
         </nav>
-        <!-- fim - navbar logado -->
+        <div id="wrapper">
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav nav-pills nav-stacked" id="menu">
+
+                    <li>
+                        <div style="float: right;"id="div_msg_logado">
+                            <?php if ($this->session->userdata("usuario_logado")['origem'] == 'FACEBOOK') : ?>
+                                <img id="img_msg_logado-face" src="<?= $this->session->userdata("usuario_logado")['picture']['data']['url'] ?>"/>
+                            <?php endif ?>
 
 
-        
+                            <?php if ($this->session->userdata("usuario_logado")['origem'] == 'GOOGLE') : ?>
+                                <img id="img_msg_logado-google" src="<?= $this->session->userdata("usuario_logado")['picture'] ?>"/>
+                            <?php endif ?>   
+                        </div>
 
+                    </li>
 
+                    <li class="sidebar-brand">
+                        <a href="<?= base_url("/index.php/login") ?>">
+                            BEM VINDO
+                            <span class="glyphicon glyphicon-home " aria-hidden="true"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url("/index.php/calculo") ?>">
+                            <span class="glyphicon glyphicon-list-alt " aria-hidden="true"></span>
+                            Calculo
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-scale " aria-hidden="true"></span>
+                            Marcação Diária
+                        </a>
 
+                    </li>
+                    <li>
+                        <a href="<?= base_url("/index.php/configuracao") ?>">
+                            <span class="glyphicon glyphicon-cog " aria-hidden="true"></span>
+                            Configurações
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-envelope " aria-hidden="true"></span>
+                            Fale Conosco
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-info-sign " aria-hidden="true"></span>
+                            Sobre
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url("index.php/login/logout") ?>">
+                            <span class="glyphicon glyphicon-log-out " aria-hidden="true"></span>
+                            Sair
+                        </a>
+                    </li>
+                </ul>
+            </div><!-- /#sidebar-wrapper -->
+
+            <!-- /#page-content-wrapper -->
+        </div>
+        <!-- /#wrapper -->
--- js_b/jscript.js
+++ js_b/jscript.js
@@ -1,29 +1,60 @@
 $(document).ready(function () {
-
-    $("#menu-toggle").click(function (e) {
+    
+    $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-
-    $(".bootstrap-switch-handle-on").html("Sim");
-    $(".bootstrap-switch-handle-off").html("Não");
-    
-    
-    
-     $("#troca-estado").click(function(){
-        var btn = $(this);
-        btn.button("loading");
-        
-        
-        
-        setTimeout(function(){
-            //btn.button("reset");
-            var kwh_relogio = $("#kwh_relogio").val();
-             window.location.href = "/index.php/calculo/calcular?kwh="+kwh_relogio;
-        }, 2000);
-        
-       
+     $("#menu-toggle-2").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-2");
+        $('#menu ul').hide();
     });
+ 
+     function initMenu() {
+      $('#menu ul').hide();
+      $('#menu ul').children('.current').parent().show();
+      //$('#menu ul:first').show();
+      $('#menu li a').click(
+        function() {
+          var checkElement = $(this).next();
+          if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            return false;
+            }
+          if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('#menu ul:visible').slideUp('normal');
+            checkElement.slideDown('normal');
+            return false;
+            }
+          }
+        );
+      }
+    $(document).ready(function() {initMenu();});
+
+    
+//    $("#menu-toggle").click(function (e) {
+//        e.preventDefault();
+//        $("#wrapper").toggleClass("toggled");
+//    });
+//
+//    $(".bootstrap-switch-handle-on").html("Sim");
+//    $(".bootstrap-switch-handle-off").html("Não");
+//    
+//    
+//    
+//     $("#troca-estado").click(function(){
+//        var btn = $(this);
+//        btn.button("loading");
+//        
+//        
+//        
+//        setTimeout(function(){
+//            //btn.button("reset");
+//            var kwh_relogio = $("#kwh_relogio").val();
+//             window.location.href = "/index.php/calculo/calcular?kwh="+kwh_relogio;
+//        }, 2000);
+//        
+//       
+//    });
  
 
 });
